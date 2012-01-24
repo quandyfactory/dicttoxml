15077516d09f28b25220f3e80f83aefa5fdc689a
Ryan McGreal <ryan@quandyfactory.com>
2012-01-24 16:44:35 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'dict2xml',
+    version = '0.1',
+    description = 'Converts a native Python dictionary into an XML string.',
+    author = 'Ryan McGreal',
+    author_email = 'ryan@quandyfactory.com',
+    licene = 'GNU General Public Licence, Version 2',
+    url = 'https://github.com/quandyfactory/dict2xml',
+    py_modules = ['dict2xml']
+)
