cf6e9371015f8d6158d2481d647e5d0a5fcdb6cc
Muriel Silveira <murielkong@gmail.com>
2015-04-18 17:45:41 -0300

    Space between method call and asserts

--- tests/test_make_id.py
+++ tests/test_make_id.py
@@ -6,8 +6,10 @@ class MakeIdTest(TestCase):
 
     def test_element_name(self):
         result = make_id('element')
+
         self.assertEqual('element_', result[:8])
 
     def test_rand_is_int(self):
         result = make_id('element')
+
         self.assertIsInstance(int(result[8:]), int)
